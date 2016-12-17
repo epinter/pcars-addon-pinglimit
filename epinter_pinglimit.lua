4cfbbe2b70bd731004f008b84a2c5bf2860c5db3
Emerson Pinter <dev@pinter.com.br>
2016-12-17 20:41:31 -0200

    Add license

--- epinter_pinglimit.lua
+++ epinter_pinglimit.lua
@@ -1,3 +1,22 @@
+--[[
+PingLimit Addon for Project Cars Dedicated Server
+
+Copyright (C) 2016  Emerson Pinter <dev@pinter.com.br>
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 3 of the License, or
+   (at your option) any later version.
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software Foundation,
+   Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
+
+--]]
+
 local addon_storage = ...
 local config = addon_storage.config
 
